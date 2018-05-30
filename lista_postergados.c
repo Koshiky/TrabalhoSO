4967dc423dda1466274d04a943673eb9d3a3638e
Koshiky <ronishino@gmail.com>
2018-05-30 03:14:49 -0300

    adicionado lista_postergados

---
+++ lista_postergados.c
@@ -0,0 +1,43 @@
+#include "utilizados.h"
+#include <time.h>
+#define SEG_POR_DIA   86400
+#define SEG_POR_HORA  3600
+#define SEG_POR_MIN   60
+
+int main() {
+
+  int idfila, job;
+  int hora, minutos;
+  long horario, aux=0;
+  struct mensagem msg;
+  struct timeval tv;
+  struct timezone tz;
+
+  idfila = msgget(KFILHO, IPC_CREAT|0600); //Owner pode ler e escrever
+
+  gettimeofday(NULL, &tz);
+
+  //Print dos Jobs
+  while(msgrcv(idfila, &msg, sizeof(struct mensagem), 0, IPC_NOWAIT) != -1){
+      if(msg.exec.job != job) {
+          printf("\n");
+          job = msg.exec.job;
+      }
+      tv = msg.exec.ini;
+      horario = tv.tv_sec % SEG_POR_DIA;
+      horario += tz.tz_dsttime * SEG_POR_HORA;
+      horario -= tz.tz_minuteswest * SEG_POR_MIN;
+      horario = (horario + SEG_POR_DIA) % SEG_POR_DIA;
+      aux = horario + msg.exec.delay;
+
+      // Separa em h:m:s
+      hora = aux / SEG_POR_HORA;
+      minutos = (aux % SEG_POR_HORA) / SEG_POR_MIN;
+
+      printf("Job: %d Executavel: %s HHMMSS: %d:%02d Copias: %d Prioridade: %d\n", msg.exec.job, msg.exec.name, hora, minutos, msg.exec.n, msg.prioridade);
+  }
+
+    msgctl(idfila, IPC_RMID, NULL);
+
+  return 0;
+}
